a606356a5f83704a066faaa9c54ac2f820d3f47b
Ramazan Dursun <ramazan.dursun06@gmail.com>
2019-09-15 21:09:37 +0300

    app

--- BankProject/Globals.cs
+++ BankProject/Globals.cs
@@ -6,7 +6,7 @@ namespace BankProject
     public class Globals
     {
         public static string isimtut = "";
-        public const string ConnectionString = "Server=192.168.56.140;Database=BankaDB;User=dert;Pwd=06319BarçaLeon*ÖşWqt";
+        public const string ConnectionString = "Server=1.1.1.1;Database=BankaDB;User=veritabaniuseradi;veritabaniusersifresiSifreniz";
     }
    
 
